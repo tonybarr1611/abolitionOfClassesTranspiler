44b6a561a765884415b5bf9f65982edea5e1c6cb
tonybarr1611 <antbarrantes@estudiantec.cr>
2025-06-29 23:38:37 -0600

    Agregar transpilación: ejemplo6_20250629_233836_950_transpilado

---
+++ ejemplo6_20250629_233836_950_transpilado/ejemplo6.py
@@ -0,0 +1,39 @@
+# Ambiente estándar para Abolition of Classes
+import sys
+
+def mostrar_mensaje(*args):
+    """Función para mostrar mensajes (equivalente a print)"""
+    mensaje = ""
+    for arg in args:
+        mensaje += str(arg)
+    print(mensaje)
+
+def opinion(texto: str = ""):
+    """Función para obtener entrada del usuario"""
+    if texto:
+        print(texto)
+    return input()
+
+def opinion_numerica(texto: str = ""):
+    """Función para obtener entrada numérica del usuario"""
+    if texto:
+        print(texto)
+    try:
+        return int(input())
+    except ValueError:
+        try:
+            return float(input())
+        except ValueError:
+            return 0
+
+# Código generado por el transpilador
+
+def es_mayor(numero1, numero2):
+    mayor = 0
+    if numero1 > numero2:
+        mayor = 1
+    else:
+        mayor = 0
+    return mayor
+resultado_mayor = 0
+resultado_mayor = es_mayor(5, 3)
